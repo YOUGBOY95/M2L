d0aec3a4da739dee95c05fc987b3fe7ad8729d88
YOUGBOY95 <86224840+YOUGBOY95@users.noreply.github.com>
2022-04-07 13:01:32 +0200

    Delete déconnexion.php

--- déconnexion.php
+++
@@ -1,8 +0,0 @@
- <?php
-session_start();
-session_destroy();
-echo "Vous êtes deconnecté.....<br>Redirection en cours...";
-header("refresh:3;url=index.php");
-//header("location:connexion.php");
-
-?>
